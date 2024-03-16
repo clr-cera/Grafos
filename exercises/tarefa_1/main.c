7005704d1a03c92c3d07cb51a8622cbec20ddcdd
clr <clrcera05@gmail.com>
2024-03-16 14:47:48 -0300

    command parser fixed and ui translated to portuguese

--- exercises/tarefa_1/main.c
+++ exercises/tarefa_1/main.c
@@ -4,10 +4,10 @@
 int main(void) {
 
   // Instruções
-  printf("z : Stop program\na : Create graph\nb : Check edge existence\nc : "
-         "Get adjcent vertices\nd : Remove edge\ne : Print graph\nf : Get "
-         "number of vertices\ng : Remove graph\nh : Add edge\ni : Remove "
-         "lightest edge\nj : Get adjacency matrix");
+  printf("z : Parar programa\na : Criar Grafo\nb : Checar existência de aresta\nc : "
+         "Pegar vértices adjacentes\nd : Remover aresta\ne : Imprimir grafo\nf : Pegar "
+         "número de vértices\ng : Remover grafo\nh : Adicionar aresta\ni : Remover "
+         "aresta com menor peso\nj : Pegar matriz de adjacência");
 
   // Variáveis
   char op;
@@ -19,77 +19,85 @@ int main(void) {
   // Parser de Instruções
   do {
     scanf("%c", &op);
-  } while (op != 'z');
-  switch (op) {
-  case 'z':
-    break;
-  case 'a': {
-    scanf("%d", &n);
-    graph = MyGraph(n);
-    break;
-  }
-  case 'b': {
-    scanf("%d %d", &v1, &v2);
-    if (exist_edge(graph, v1, v2)) {
-      printf("Exist edge");
-    } else {
-      printf("Do not exist edge");
-    }
-    break;
-  }
-  case 'c':
-    scanf("%d", &v1);
-    {
-      array = get_adj_vertex(graph, v1);
-      for (int i = 0; array[i] != -1; i++) {
-        printf("%d ", array[i]);
+
+    switch (op) {
+      case 'z':
+        break;
+
+      case 'a': {
+        printf("Digite o número de vértices: ");
+        scanf("%d", &n);
+        graph = MyGraph(n);
+        break;
       }
-      break;
+
+      case 'b': {
+        printf("Digite os vértices da aresta, com um espaço os separando: ");
+        scanf("%d %d", &v1, &v2);
+        if (exist_edge(graph, v1, v2)) {
+          printf("Existe aresta.\n");
+        } else {
+          printf("Não existe aresta.\n");
+        }
+        break;
+      }
+
+      case 'c': {
+        printf("Digite o vértice: ");
+        scanf("%d", &v1);
+        array = get_adj_vertex(graph, v1);
+        for (int i = 0; array[i] != -1; i++) {
+          printf("%d ", array[i]);
+        }
+        break;
+      }
+
+      case 'd': {
+        printf("Digite vértices da aresta a ser removida, com um espaço os separando: ");
+        scanf("%d %d", &v1, &v2);
+        if (remove_edge(graph, v1, v2)) {
+          printf("Remoção concluída.\n");
+        } else {
+          printf("O vértice não existia\n");
+        }
+        break;
+      }
+      case 'e': {
+        print_info(graph);
+        break;
+      }
+      case 'f': {
+        num_vertex = number_of_vertexs(graph);
+        printf("%d", num_vertex);
+        break;
+      }
+      case 'g': {
+        if (remove_graph(graph)) {
+          printf("Grafo foi removido.\n");
+        } else {
+          printf("Grafo não existia.\n");
+          break;
+        }
+      }
+      case 'h': {
+        scanf("%d %d %d", &v1, &v2, &weight);
+        if (add_edge(graph, v1, v2, weight)) {
+          printf("Adicionou aresta.\n");
+        } else {
+          printf("Grafo não existe.\n");
+        }
+        break;
+      }
+      case 'i':
+        printf("A aresta com menor peso %d foi removida.\n",
+               remove_with_least_weight(graph));
+        break;
+      case 'j':
+        matrix = adjacency_matrix(graph);
+        break;
+      default:
+        break;
     }
-  case 'd': {
-    scanf("%d %d", &v1, &v2);
-    if (remove_edge(graph, v1, v2)) {
-      printf("Remove edge completed");
-    } else {
-      printf("Something went wrong");
-    }
-    break;
-  }
-  case 'e': {
-    print_info(graph);
-    break;
-  }
-  case 'f': {
-    num_vertex = number_of_vertexs(graph);
-    printf("%d", num_vertex);
-    break;
-  }
-  case 'g': {
-    if (remove_graph(graph)) {
-      printf("Remove graph completed");
-    } else {
-      printf("Something went wrong");
-      break;
-    }
-  }
-  case 'h': {
-    scanf("%d %d %d", &v1, &v2, &weight);
-    if (add_edge(graph, v1, v2, weight)) {
-      printf("Add edge successful");
-    } else {
-      printf("Something went wrong");
-    }
-    break;
-  }
-  case 'i':
-    printf("The least weight edge %d was removed",
-           remove_with_least_weight(graph));
-    break;
-  case 'j':
-    matrix = adjacency_matrix(graph);
-    break;
-  default:
-    break;
-  }
+  } while (op != 'z');
   return 0;
 }
